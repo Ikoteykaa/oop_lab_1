35c9c4f4db21039fb757eb0bc3f80ba9932e963e
K_Anastasia <kindeevaanastasia68@gmail.com>
2026-09-06 16:27:42 +0300

    add class

---
+++ Video_games.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_lab_1
+{
+    internal class Video_games
+    {
+    }
+}
